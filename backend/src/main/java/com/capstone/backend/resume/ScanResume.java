0b14bc70fb6e746165b5c1d8eadc81045eeef0a0
surendra <surendra.rules@gmail.com>
2024-08-25 02:13:43 -0500

    resume api

--- backend/src/main/java/com/capstone/backend/resume/ScanResume.java
+++ backend/src/main/java/com/capstone/backend/resume/ScanResume.java
@@ -23,8 +23,8 @@ public class ScanResume {
 
     public ResponseEntity<String> callPythonApi(MultipartFile resumeBrowse, String job_description) {
     	try {
-    		String url = "http://localhost:5000/performSimilarityMatch";
-    		//String url = "https://backend-pf-0b1e7c97ff65.herokuapp.com/performSimilarityMatch";
+    		//String url = "http://localhost:5000/performSimilarityMatch";
+    		String url = "https://backend-pf-0b1e7c97ff65.herokuapp.com/performSimilarityMatch";
     		
     		// Prepare headers
             HttpHeaders headers = new HttpHeaders();
