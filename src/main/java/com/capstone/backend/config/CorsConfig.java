9e52e82c5116c922a58f21d4746ee0a735a2ce17
surendra <surendra.rules@gmail.com>
2024-08-25 11:32:51 -0500

    Remove incorrect backend subtree

--- src/main/java/com/capstone/backend/config/CorsConfig.java
+++ src/main/java/com/capstone/backend/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.capstone.backend.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://lovely-squirrel-48c46f.netlify.app")
+                .allowedOrigins("https://lovely-squirrel-48c46f.netlify.app", "https://backend-pf-0b1e7c97ff65.herokuapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
