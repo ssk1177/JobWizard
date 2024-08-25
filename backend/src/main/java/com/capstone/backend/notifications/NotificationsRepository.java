c8b5fa0c0b115b73536a4cd93ef2cca1dda671f6
surendra <surendra.rules@gmail.com>
2024-08-25 10:58:01 -0500

    Remove cached backend subtree to reinitialize

--- backend/src/main/java/com/capstone/backend/notifications/NotificationsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.backend.notifications;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface NotificationsRepository extends CrudRepository<Notifications, Long>{
-
-	Notifications findByUserName(String username);
-}
