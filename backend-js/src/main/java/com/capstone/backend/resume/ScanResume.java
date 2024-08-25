f830c065959ba799769578233e61b7a1384e8bd4
surendra <surendra.rules@gmail.com>
2024-08-25 16:08:27 -0500

    recovered python code

--- backend-js/src/main/java/com/capstone/backend/resume/ScanResume.java
+++ backend-js/src/main/java/com/capstone/backend/resume/ScanResume.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @Service
 public class ScanResume {
