7c0630117e6ba39fd46a941ca5bcee9c79641384
surendra <surendra.rules@gmail.com>
2024-08-25 12:32:00 -0500

    renamed the project

--- backend-js/src/main/java/com/capstone/backend/userDetails/UserDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capstone.backend.userDetails;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserDetailsRepository extends CrudRepository<UserDetails, Long>{
-
-	UserDetails findByUserName(String username);
-}
